f018bf444099583c4a267de60742ba2067f7ddd2
Moviesuploader <138097021+Moviesuploader@users.noreply.github.com>
2024-11-12 18:56:09 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -286,7 +286,7 @@ def get_readable_message():
         else:
             msg += f"<blockquote>📁Size: {download.size()}"
         msg += f"\n⏳️Elapsed: {get_readable_time(time() - download.message.date.timestamp())}</blockquote>"
-        msg += f"\n❌️<blockquote>/stop_{download.gid()[:8]}</blockquote>\n\n"
+        msg += f"\n<blockquote>❌️/stop_{download.gid()[:8]}</blockquote>\n\n"
     if len(msg) == 0:
         return None, None
     if tasks > STATUS_LIMIT:
