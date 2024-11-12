371b7d64488c4ecdfcb2615814b6228ba4f1d997
Moviesuploader <138097021+Moviesuploader@users.noreply.github.com>
2024-11-12 18:47:03 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -248,7 +248,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = "<b>⚡️𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔: <a href="https://telegram.me/btmx_zone">𝘽𝙏𝙈𝙓🔱</a></b>'
+    msg = "<b>⚡️𝙋𝙊𝙒𝙀𝙍𝙀𝘿 𝘽𝙔: <a href="https://telegram.me/btmx_zone">𝘽𝙏𝙈𝙓🔱</a></b>\n\n"
     button = None
     tasks = len(download_dict)
     current_time = get_readable_time(time() - bot_start_time)
@@ -263,7 +263,7 @@ def get_readable_message():
     for download in list(download_dict.values())[
         STATUS_START : STATUS_LIMIT + STATUS_START
     ]:
-        msg += f"<b>🏷{download.status()}:</b> {escape(f'{download.name()}')}\n"
+        msg += f"<b>{download.status()}:</b>🏷 {escape(f'{download.name()}')}\n"
         msg += f"👉by {source(download)}\n"
         if download.status() not in [
             MirrorStatus.STATUS_SPLITTING,
